866efd65a34a7d76352eba063cd30bab741feb33
BlessingAdewale <adelekeblessingadewale@gmail.com>
2025-02-04 17:31:34 +0100

    refactor:  improve boilerplate

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,14 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
-const Explore = () => {
+const tracking = () => {
   return (
     <SafeAreaView>
-      <Text>explore</Text>
+      <Text>tracking</Text>
     </SafeAreaView>
   )
 }
 
-export default Explore
+export default tracking
 
 const styles = StyleSheet.create({})
